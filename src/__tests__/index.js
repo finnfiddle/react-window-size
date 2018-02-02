103a72810561090d322c5bc0e67aaa9606c0a2b0
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2018-02-02 22:13:21 +0200

    added tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,23 +1,35 @@
 /* eslint-disable prefer-arrow-callback, react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React, { Component } from 'react';
-import { shallow } from 'enzyme';
+const jsdom = require('jsdom').jsdom;
+const exposedProperties = ['window', 'navigator', 'document'];
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+// documentRef = document;
+
+import React from 'react';
+import { mount } from 'enzyme';
 import { assert } from 'chai';
 import windowSize from '../index';
 
 const ScreenSize = (props) => (
-  <p>
-    Screen width is: {props.windowWidth}
-    <br />
-    Screen height is: {props.windowHeight}
-  </p>
+  <div>{`===${props.windowWidth}===${props.windowHeight}===`}</div>
 );
 
 describe('windowSize', () => {
 
   it('should have a display name with the name of the composed component', () => {
     const EnhancedComponent = windowSize(ScreenSize);
-    const wrapper = shallow(<div><EnhancedComponent /></div>);
+    const wrapper = mount(<div><EnhancedComponent /></div>);
     assert.equal(wrapper.children().at(0).name(), `windowSize(${ScreenSize.name})`);
   });
 
@@ -25,8 +37,22 @@ describe('windowSize', () => {
     ScreenSize.displayName = 'p';
     try {
       const EnhancedComponent = windowSize(ScreenSize);
-      const wrapper = shallow(<div><EnhancedComponent /></div>);
+      const wrapper = mount(<div><EnhancedComponent /></div>);
       assert.equal(wrapper.children().at(0).name(), `windowSize(${ScreenSize.displayName})`);
+    }
+    finally {
+      delete ScreenSize.displayName;
+    }
+  });
+
+  it('should get passed the width and height', () => {
+    ScreenSize.displayName = 'p';
+    try {
+      const EnhancedComponent = windowSize(ScreenSize);
+      const wrapper = mount(<div><EnhancedComponent /></div>);
+      assert.equal(wrapper.contains(
+        <div>{'===1024===768==='}</div>
+      ), true);
     }
     finally {
       delete ScreenSize.displayName;
